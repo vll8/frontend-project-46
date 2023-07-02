deaad504cdc11c1ca22d8a73bd34450e0abdb16b
Vladimir L <lototskiy.vladimir@mail.ru>
2023-07-02 11:39:00 +0600

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,9 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .option('-V, --version', 'output the version number')
+    .argument('<filepath1>', 'first file')
+    .argument('<filepath2>', 'second file')
+    .option('-f, --format <type>', 'output format')
+    
 
     program.parse();
