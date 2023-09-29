4e048236566aed599108ab81d51179cbbcb2be3f
Vladimir L <lototskiy.vladimir@mail.ru>
2023-09-29 16:10:06 +0600

    new step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,8 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2');
 program.parse();
